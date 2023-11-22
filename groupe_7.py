b920badd5eb9f2aa3db2f9f2ea02970bc5c7da46
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2023-11-22 13:56:57 +0100

    fucntions comments

--- groupe_7.py
+++ groupe_7.py
@@ -1,11 +1,27 @@
 import numpy as np
 
-def pageRankLinear (A : np.matrix , alpha : float , v : np.array ) -> np.array:
+def pageRankLinear (A : np.matrix , alpha : float, v : np.array ) -> np.array:
+    """
+    `A` : np.matrix : matrice d'adjacence
+    `alpha` : float : paramètre de téléportation (entre 0 et 1)
+    `v` : np.array : vecteur de probabilité initiale
+
+    Retourne le vecteur de probabilité stationnaire du PageRank -> Un vecteur `x` contenant les scores d’importance des noeuds ordonnés dans
+    le même ordre que les lignes de la matrice d’adjacence (représentant les noeuds).
+    """
     n = A.shape[0]
     B = alpha*A + (1-alpha)*np.ones((n,n))/n
     return np.linalg.solve(B,v)
 
-def pageRankPower (A : np.matrix , alpha : float , v : np . array ) -> np.array:
+def pageRankPower (A : np.matrix, alpha : float, v : np . array ) -> np.array:
+    """
+    `A` : np.matrix : matrice d'adjacence
+    `alpha` : float : paramètre de téléportation (entre 0 et 1)
+    `v` : np.array : vecteur de probabilité initiale
+
+    Retourne le vecteur de probabilité stationnaire du PageRank -> Un vecteur `x` contenant les scores d’importance des noeuds ordonnés dans
+    le même ordre que les lignes de la matrice d’adjacence (représentant les noeuds).
+    """
     n = A.shape[0]
     B = alpha*A + (1-alpha)*np.ones((n,n))/n
     x = v
