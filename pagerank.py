d34a1ff3e79f2db6690108f0cfd95b4451e56f2f
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2023-12-21 16:44:23 +0100

    ça marche pas

--- pagerank.py
+++ pagerank.py
@@ -21,7 +21,7 @@ def pageRankLinear (A : np.matrix , alpha : float, v : np.array ) -> np.array:
 
     return x
 
-def pageRankPower (A : np.matrix, alpha : float, v : np . array ) -> np.array:
+def pageRankPower (A : np.matrix, alpha : float, v : np.array ) -> np.array:
     """
     `A` : np.matrix : matrice d'adjacence
     `alpha` : float : paramètre de téléportation (entre 0 et 1)
@@ -30,23 +30,27 @@ def pageRankPower (A : np.matrix, alpha : float, v : np . array ) -> np.array:
     Retourne le vecteur de probabilité stationnaire du PageRank -> Un vecteur `x` contenant les scores d’importance des noeuds ordonnés dans
     le même ordre que les lignes de la matrice d’adjacence (représentant les noeuds).
     """
+    print(A)
+
+    # Création de la matrice de probabilité de transition P
     n = A.shape[0]
-    I = np.identity(n)
+    P = np.zeros((n, n))
 
-    # Normalize the adjacency matrix
-    A_normalized = A / A.sum(axis=1)
+    # Assignation de p_ij = w_ij / nombre de liens sortants de j
+    for i in range(n):
+        for j in range(n):
+            if A[i,j] != 0:
+                Lj = A[:,j].sum() # Nombre de liens sortants de j
+                P[i,j] = A[i,j] / Lj
 
-    # Initialize PageRank vector
-    x = v
+    print(P)
 
-    for _ in range(1000):
-        x_new = (1 - alpha) * (A_normalized.T @ x) + alpha * v
+    # fait la somme de toutes les colonnes de M
+    # print("M est stochastique : ", np.allclose(M.sum(axis=0), np.ones(n)))
 
-        # Check for convergence
-        if np.linalg.norm(x_new - x, ord=1) < 1e-6:
-            break
+    # Calcul de la matrice Google G
+    G = alpha * P + (1 - alpha) * v
 
-        x = x_new
+    print(G)
 
-    return x
-
+    
